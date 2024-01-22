09113cff93e71602217b3ebc709a3367cd653a0e
NPierce1798 <npierce1798@gmail.com>
2024-01-22 15:53:57 -0500

    fixes

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -8,7 +8,7 @@ function Hero() {
       
             <p className="hero__heading">Build Your Dreams With Us</p>
             <p className="hero__p">Schedule A Free Project Estimate Today!</p>
-            <button className="hero__button">Schedule A Session!</button>
+            <button href='https://calendly.com/justaskcodes23/30min' target="_blank" className="hero__button" >Schedule A Session!</button>
       
         </div>
 
